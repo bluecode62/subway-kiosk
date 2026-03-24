226fe3611456ce1e3776375e85aadce7b156a978
DESKTOP-EMC9DKP\COM <bluecode622>
2026-03-24 17:08:59 +0900

    build 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
@@ -8,14 +8,14 @@ import Paid from "./pages/Paid";
 function App() {
   return (
     <div className="App">
-      <div className="kiosk">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/paid" element={<Paid />} />
-        </Routes>
-      </div>
+        <div className="kiosk">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/paid" element={<Paid />} />
+            </Routes>
+        </div>
     </div>
   );
 }
